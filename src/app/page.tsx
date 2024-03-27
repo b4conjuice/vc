2f2a31440d294d165a1f776cbbd6f60687ddf887
Daniel Lopez <dlopez807@gmail.com>
2024-03-26 21:24:30 -0700

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,58 @@
+import Main from '@/app/_components/main'
+import fetcher from '@/lib/fetcher'
+import Title from '@/app/_components/title'
+
+type Deployment = {
+  alias: string[]
+}
+
+type Project = {
+  id: string
+  name: string
+  latestDeployments: Deployment[]
+}
+
+const PROJECT_URL = 'https://api.vercel.com/v9/projects'
+// const USER_URL = 'https://api.vercel.com/v2/user'
+
+export default async function Home() {
+  const { projects }: { projects: Project[] } = await fetcher(PROJECT_URL, {
+    headers: {
+      Authorization: `Bearer ${process.env.TOKEN!}`,
+    },
+    method: 'get',
+  })
+  return (
+    <Main className='flex flex-col p-4'>
+      <div className='flex flex-grow flex-col space-y-4'>
+        <Title>home</Title>
+        <ul className='divide-cb-dusty-blue divide-y'>
+          {projects.map(p => {
+            const { id, name, latestDeployments } = p
+            const alias =
+              latestDeployments && latestDeployments !== undefined
+                ? latestDeployments[0]?.alias[0]
+                : ''
+            const url = alias ? `https://${alias}` : null
+            return (
+              <li key={id} className='flex items-center py-4 first:pt-0'>
+                {alias && url ? (
+                  <a
+                    href={url}
+                    className='text-cb-pink hover:text-cb-pink/75 flex w-full'
+                    target='_blank'
+                  >
+                    <span className='grow'>{name}</span>
+                    <span>{alias}</span>
+                  </a>
+                ) : (
+                  <span>{name}</span>
+                )}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </Main>
+  )
+}
